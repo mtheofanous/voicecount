77663cb8904864c3724cff79442495f87cb798a1
Martheo <theofanousmario@gmail.com>
2026-02-19 21:28:28 +0100

    hg

--- features/manage_orders/orders.py
+++ features/manage_orders/orders.py
@@ -1711,7 +1711,7 @@ def _render_lines_editor(*, venue_id: int, order: Order, actor: str, products: l
                 "product_label",
                 header_name="Producto",
                 editable=False,
-                minWidth=300,
+                minWidth=250,
                 flex=2
             )
 
